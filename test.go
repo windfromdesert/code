eef3eb0487ed1848fa144ed44f9d0d2806e018de
wangbin <wbwuxi@gmail.com>
2014-03-18 20:05:30 +0800

    deleted three files.

--- test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func  main()  {
-    for k := 0 ; k < 5; k++ {
-        fmt.Printf("%d", k)
-    }
-}
--- test.py
+++
@@ -1,4 +0,0 @@
-import cPickle as cP
-f = file('g:/doc/myaddress')
-st = cP.load(f)
-print st
